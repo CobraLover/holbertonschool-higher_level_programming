a1a3855df36d395ad231b6c29122b82bccfffb45
CobraLover <6716@holbertonstudents.com>
2024-01-26 07:55:12 -0800

    Task.0-9

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+
+    names = dir(hidden_4)
+    for name in range(0, len(names)):
+        if names[name][:2] != "__":
+            print("{:s}".format(names[name]))
